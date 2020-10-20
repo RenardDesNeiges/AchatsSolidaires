42d850fcb148ffdf0e505c56e4471d8d6f8b5e47
salutctitou <titouan.renard@gmail.com>
2020-10-20 19:39:14 +0200

    Accounting done

--- plugin_files/js/generate_order.js
+++ plugin_files/js/generate_order.js
@@ -1,3 +1,8 @@
+/* 
+ *  Utility functions
+*/
+
+// Downloads a string as a text file
 function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -10,7 +15,7 @@ function download(filename, text) {
 
   document.body.removeChild(element);
 }
-
+// Queries GraphDB
 function dbQuery(inputQuery,promiseFunction){
     //Computes the url of the graphql server
     local_url = "https://" + window.location.hostname + "/graphql"
@@ -25,36 +30,101 @@ function dbQuery(inputQuery,promiseFunction){
     .then(res => promiseFunction(res.data));
 }
 
+/* 
+ *  DB query formatting functions
+*/
 
-cashSummaryGenerator = function(res){
-    console.log("Log")
-    orders_array = res.orders.nodes
-    //document.getElementById('mainP').innerHTML = cashSummaryToCSV(orders_array)
-    download("accounting.csv",cashSummaryToCSV(orders_array))
-    console.log(orders_array)
+//Converts the json from the DB to an accounting-ready csv (pending orders)
+function pendingAccountingCSV(inputArray){
 
-}
-
-function cashSummaryToCSV(inputArray){
-    //Name line
-    buffer =  "Prénom/Nom,Total Commande,\n"
+    //Names of the collumns
+    buffer =  "Prénom/Nom,Total Commande,date\n"
     //Content
     for(element in inputArray){
-        buffer += inputArray[element].customer.firstName + " " + inputArray[element].customer.lastName + ","
-        buffer += inputArray[element].subtotal + ","
-        buffer += "\n"
+        if(inputArray[element].status == "PROCESSING"){
+            buffer += inputArray[element].customer.firstName + " " + inputArray[element].customer.lastName + ","
+            buffer += inputArray[element].subtotal + ","
+            buffer += inputArray[element].date.substring(0, 10)
+            buffer += "\n"
+        }
     }
-
 
     return buffer
 }
 
-cashSummaryRequest = " query MyQuery { orders { nodes {  subtotal customer { firstName lastName } } } }";
+//Converts the json from the DB to an accounting-ready csv (pending orders)
+function accountingCSV(inputArray){
 
-function generateCashCSV(){
-    dbQuery(cashSummaryRequest,cashSummaryGenerator)
+    
+    
+    inputval = document.getElementById("start-date").value;
+    zero_date = new Date(inputval);
+
+    //Names of the collumns
+    buffer =  "Prénom/Nom,Total Commande,date\n"
+    //Content
+    for(element in inputArray){
+        cdate = new Date(inputArray[element].date.substring(0, 10))
+        if(cdate >= zero_date){
+            buffer += inputArray[element].customer.firstName + " " + inputArray[element].customer.lastName + ","
+            buffer += inputArray[element].subtotal + ","
+            buffer += inputArray[element].date.substring(0, 10) + ","
+            buffer += inputArray[element].status
+            buffer += "\n"
+        }
+    }
+
+    return buffer
 }
 
+//Gets the price per-person for accounting purposes (pending orders)
+pendingAccounting = function(res){
+    console.log("Log")
+    orders_array = res.orders.nodes
+
+    var today = new Date();
+    var filename = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+"_pending_accounting.csv";
+
+    download(filename,pendingAccountingCSV(orders_array))
+    console.log(orders_array)
+
+}
+
+accounting = function(res){
+    console.log("Log")
+    orders_array = res.orders.nodes
+
+    var today = new Date();
+    var filename = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+"_accounting.csv";
+
+    download(filename,accountingCSV(orders_array))
+    console.log(orders_array)
+
+}
+
+/* 
+ *  GraphQL requests
+*/
+
+//Request for the accounting csv
+cashSummaryRequest = " query MyQuery { orders { nodes { subtotal customer { firstName lastName } date status } } }";
+
+/* 
+ *  "Main" functions
+*/
+
+function generatePendingAccountingCSV(){
+    dbQuery(cashSummaryRequest,pendingAccounting)
+}
+
+function generateAccountingCSV(){
+    dbQuery(cashSummaryRequest,accounting)
+}
+
+// Runs when the page is loaded
 jQuery(document).ready(function($) {
-    //generateCashCSV()
+    //binding buttons to functions
+    document.getElementById("generatePeriodAccounting").onclick = generatePendingAccountingCSV;
+    document.getElementById("generateAccounting").onclick = generateAccountingCSV;
 });
+
--- plugin_files/order_manager_template.php
+++ plugin_files/order_manager_template.php
@@ -11,6 +11,14 @@ wp_enqueue_script('generate_order');
 get_header() 
 ?>
 
-<button type="button"  onclick="generateCashCSV()">Generate Accounting Summary</button>
+<h3>Compatbilité : </h3>
+<button type="button"  id="generatePeriodAccounting">Générer la comptabilité pour les commandes en cours</button>
+</br>
+<button type="button"  id="generateAccounting">Générer la comptabilité à partir du : </button>
+<input type="date" id="start-date" name="account-start"
+       value="2019-01-01"
+       min="2018-01-01">
+</br>
+
 
 <div id="mainP"></div>
