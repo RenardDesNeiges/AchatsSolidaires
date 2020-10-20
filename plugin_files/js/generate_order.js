80f4558c489212928d9398f9d06170857d2f91c3
salutctitou <titouan.renard@gmail.com>
2020-10-20 16:33:44 +0200

    GraphDB Request kind of working

--- plugin_files/js/generate_order.js
+++ plugin_files/js/generate_order.js
@@ -1,4 +1,42 @@
+function dbQuery(inputQuery,promiseFunction){
+    //Computes the url of the graphql server
+    local_url = "https://" + window.location.hostname + "/graphql"
+    
+    //fetches the query
+    const resp_db = fetch(local_url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: inputQuery }),
+    })
+    .then(res => res.json())
+    .then(res => promiseFunction(res.data));
+}
+
+
+cashSummaryGenerator = function(res){
+    console.log("Log")
+    console.log(res)
+}
+
+cashSummaryRequest = "  query MyQuery {
+                        orders {
+                            nodes {
+                            subtotal
+                            id
+                            customer {
+                                displayName
+                            }
+                            }
+                        }
+                    }";
+
+function callDB(){
+    
+    //test_query = "{ orders { edges { node { id } } } }"
+    dbQuery(cashSummaryRequest,cashSummaryGenerator)
+}
 
 jQuery(document).ready(function($) {
-  document.getElementById('mainP').innerHTML = "THIS IS SET BY MY VERY COOL SCRIPT"
+    document.getElementById('mainP').innerHTML = window.location.hostname
+    callDB()
 });
